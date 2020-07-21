833078a0c267a5990a58e5ff3c5aae9a67b53dd4
Julio Cesar <cesarj598@gmail.com>
2020-07-21 17:42:24 -0300

    CheckMate Logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -5,14 +5,14 @@ import boardgame.Piece;
 import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
-	
+
 	private Color color;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
 	}
-	
+
 	public Color getColor() {
 		return color;
 	}
@@ -20,7 +20,7 @@ public abstract class ChessPiece extends Piece {
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
-
+	
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -3,20 +3,18 @@ package chess;
 import boardgame.Position;
 
 public class ChessPosition {
-
+	
 	private char column;
 	private int row;
+	
 	public ChessPosition(char column, int row) {
-		
-		if(column < 'a' || column > 'h' || row < 1 || row > 8) {
-			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8");
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
 		}
-		
 		this.column = column;
 		this.row = row;
-		
 	}
-	
+
 	public char getColumn() {
 		return column;
 	}
@@ -37,7 +35,4 @@ public class ChessPosition {
 	public String toString() {
 		return "" + column + row;
 	}
-	
-	
-	
 }
